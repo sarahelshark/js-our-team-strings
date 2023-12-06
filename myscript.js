46514d30a67004712dc7bf00e6efa959665a5502
sarahelshark <sarahelsh183@gmail.com>
2023-12-06 21:21:51 +0100

    feat add 1st milestone

---
+++ myscript.js
@@ -0,0 +1,20 @@
+/*Utilizzando i dati forniti, creare un array di stringhe con i nomi dei membri del team.
+MILESTONE 0:
+Creare l’array di stringhe contenente i nomi dei membri del team.
+MILESTONE 1:
+Stampare le informazioni su DOM come card.
+MILESTONE 3:
+Aggiungere un evento click sulla card che aggiunge/rimuove una classe per evidenziare un componente del team.
+
+BONUS 1:
+In generale curare la parte di UI e organizzare i singoli membri in card/schede.
+BONUS 2:
+Inserire un form per l’aggiunta di un elemento alla lista.
+*/
+
+//arrai di stringhe contenente nome dei membri del team
+const listTeam  = ["Tommy", "Max", "Milky", "Finn", "Jake", "Gabibbo"];
+for (let i = 0; i < listTeam.length; i++){
+     const teamMate = listTeam [i];
+     console.log(teamMate);//loggo per vederlo 
+}
