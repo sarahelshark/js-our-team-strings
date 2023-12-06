701ad0ac752f77a73ef1eca596ab4ac67dcb030b
sarahelshark <sarahelsh183@gmail.com>
2023-12-06 22:11:46 +0100

    Feat add function addCards

--- myscript.js
+++ myscript.js
@@ -11,10 +11,38 @@ In generale curare la parte di UI e organizzare i singoli membri in card/schede.
 BONUS 2:
 Inserire un form per l’aggiunta di un elemento alla lista.
 */
-
-//arrai di stringhe contenente nome dei membri del team-milestone 0 
+//milestone 0
+//arrai di stringhe contenente nome dei membri del team
 const listTeam  = ["Tommy", "Max", "Milky", "Finn", "Jake", "Gabibbo"];
 for (let i = 0; i < listTeam.length; i++){
      const teamMate = listTeam [i];
      console.log(teamMate);//loggo per vederlo 
+
+
+//milestone 1
+//racchiudo dentro funzione che mi crea le 6 carte ? 
+function addCards() { // ()teamMate, listTeam)dichiaro i parametri per il momento di manipolazione elementi array successivo
+//creo un div con classe col-3, ancora vuoto di contenuti 
+     const cardElement = document.createElement('div');
+     cardElement.className = "col-4 card p-4";
+     //console.log(cardElement.outerHTML);//loggo per vederlo
+
+     //aggiungo dei contenuti di prova
+     const cardContent = document.createTextNode(listTeam);
+     console.log(cardContent); //da cambiare con i nomi della array!!!!!! e B
+     //inserisco il nodo testuale dentro il div
+     cardElement.appendChild(cardContent);
+//inserisco tutto nella dom 
+document.querySelector(".row.p-3.flex-row").appendChild(cardElement);
+return;
 }
+
+addCards()//richiamo la funzione per vedere se funziona 
+
+}
+
+ 
+   
+  //B//inserisco al suo interno un div con classe card e dentro nome+foto del membro del team + descrizione del compito
+        
+       
